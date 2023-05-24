dedd732e266a3d7b30a37f5d00d0149a4467f356
Jordy <jordycorporan95@gmail.com>
2023-05-23 21:04:37 -0400

    making progress

--- W4D3/practice-for-ch-ruby-simon-main/lib/simon.rb
+++ W4D3/practice-for-ch-ruby-simon-main/lib/simon.rb
@@ -4,24 +4,43 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    take_turn unless game_over
+    game_over_message && reset_game
+
+
+
+
+
   end
 
   def take_turn
+    show_sequence
+    require_sequence
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
+    add_random_color
   end
 
   def require_sequence
   end
 
   def add_random_color
+    @seq << COLORS.sample
   end
 
   def round_success_message
+    p ""
   end
 
   def game_over_message
