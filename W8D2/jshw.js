34650c96cf9f8139899a357f3dbf2b0e47d63b1b
Jordy <jordycorporan95@gmail.com>
2023-06-20 08:25:56 -0400

    Using the terminal to test JS

--- W8D2/jshw.js
+++ W8D2/jshw.js
@@ -1,3 +1,13 @@
 function NBAPlayer(name,team,pos){
-    this.name = name
+    this.name = name,
+    this.team = team,
+    this.pos = pos
 }
+
+const curry = new NBAPlayer ()
+
+NBAPlayer.prototype.dunk = function() {};
+
+curry.dunk();
+
+curry
