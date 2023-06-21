044dbf2728ee29bfdf10f539616b4c07453fd0ed
Jordy <jordycorporan95@gmail.com>
2023-06-21 08:47:08 -0400

    Completed first problem

--- W8D3/practice-for-ch-js-intro-js-problems2-main/Additional_JS_Intro_Problems.js
+++ W8D3/practice-for-ch-js-intro-js-problems2-main/Additional_JS_Intro_Problems.js
@@ -1,4 +1,3 @@
-
 // function titleize(name,Callback) {
 //     this.name = name
 //     Array.prototype.map; {
@@ -7,11 +6,20 @@
 //     }
 
 // }
-function titleize(names,callback) {
-    var titleizeNames = names.map(function(name) {
+function titleize(names, callback) {
+    var titleizeNames = names.map(function (name) {
         return "Mx. " + name + " Jingleheimer Schmidt";
 
     });
+    titleizeNames.forEach(callback);
 
-    
+
 }
+
+
+function printCallback(name) {
+    console.log(name);
+}
+
+
+titleize(["Mary", "Brian", "Leo"], printCallback);
