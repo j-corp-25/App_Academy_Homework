55987771c60cc90d18601c1b1fae04f1594aa4ef
Jordy <jordycorporan95@gmail.com>
2023-06-21 08:40:03 -0400

    Trying to work with callbacks

---
+++ W8D3/practice-for-ch-js-intro-js-problems2-main/Additional_JS_Intro_Problems.js
@@ -0,0 +1,17 @@
+
+// function titleize(name,Callback) {
+//     this.name = name
+//     Array.prototype.map; {
+
+
+//     }
+
+// }
+function titleize(names,callback) {
+    var titleizeNames = names.map(function(name) {
+        return "Mx. " + name + " Jingleheimer Schmidt";
+
+    });
+
+    
+}
