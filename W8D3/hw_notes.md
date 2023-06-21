b6ba0203ee9d2bceb8bde7aac01a6d5a9fe2db1d
Jordy <jordycorporan95@gmail.com>
2023-06-20 20:32:29 -0400

    Going over Constructors

---
+++ W8D3/hw_notes.md
@@ -0,0 +1,4 @@
+# HW W8D3
+
+ in js you cant declare a variable without using a
+ `var` , `const` or `let`
--- closures.js
+++ closures.js
@@ -31,18 +31,82 @@ function sum(nums){
 
 
 
-function createCounter() {
-    let count = 0;
+// function createCounter() {
+//     let count = 0;
 
-    return () => ++count;
+//     return () => ++count;
+// }
+
+
+// let counter = createCounter();
+// console.log(counter()); // => 1
+// console.log(counter()); // => 2
+// counter.count; // undefined
+// let counter2 = createCounter();
+// console.log(counter2()); // => 1
+// console.log(counter2()); // => 1
+// console.log(counter2()); // => 1
+
+
+
+//! Why is this not working???
+
+function Counter () {
+    this._count = 5;
+
+    Counter.prototype.fire = function () {
+        this._count += 1;
+        return this._count;
+    }
 }
 
+let counter = new Counter();
+counter.fire(); // 1
+counter.fire(); // 2
+counter._count // 2
+counter._count = 8 //! this should return 0 but it does not
 
-let counter = createCounter();
-console.log(counter()); // => 1
-console.log(counter()); // => 2
-counter.count; // undefined
-let counter2 = createCounter();
-console.log(counter2()); // => 1
-console.log(counter2()); // => 1
-console.log(counter2()); // => 1
+
+
+
+
+// function Counter () {
+//     this._count = 0;
+//   }
+
+//   Counter.prototype.fire = function () {
+//     this._count += 1;
+//     return this._count;
+//   }
+
+//   let counter = new Counter();
+//   counter.fire(); // 1
+//   counter.fire(); // 2
+//   counter._count // 2
+//   counter._count = 0 // 0 (this works);
+
+
+function showName(firstName,lastName) {
+    let nameIntro= "Your name is ";
+
+    function makeFullName () {
+        return nameIntro + firstName + " " + lastName;
+      }
+      return makeFullName ();
+}
+
+console.log(showName ("Michael", "Jackson"));
+
+
+
+function Kitten(name, age) {
+    this.name = name;
+    this.age = age;
+
+    this.meow = function () {
+        console.log(this.name + `says "meow!"`);
+    };
+}
+
+let Kitten = new Kitten("Earl", 2);
+
