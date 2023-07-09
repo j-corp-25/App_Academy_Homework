ca16ef63441ebab476b5241a3633ece92537d2f1
Jordy <jordycorporan95@gmail.com>
2023-07-09 17:20:45 -0400

    Useeffect tuto

--- W11D1/useState.js
+++ W11D1/useState.js
@@ -50,3 +50,50 @@ const UseState = () => {
 
 export default UseState;
 
+
+
+import { useEffect, useState } from "react";
+
+const Main = () => {
+  const [toggleOne, setToggleOne] = useState(false);
+  const [toggleTwo, setToggleTwo] = useState(false);
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setToggleOne(!toggleOne);
+  };
+  const handleClick2 = () => {
+    setToggleTwo(!toggleTwo);
+  };
+  useEffect(() => {
+    console.log("UseEffect1 Ran");
+  }, [toggleOne]);
+  useEffect(() => {
+    console.log("UseEffect2 Ran");
+    if (toggleTwo)
+      console.log("toggleTwo slice of state is true so this code runs");
+  }, [toggleTwo]);
+
+  useEffect(() => {
+    const myInterval = setInterval(() => {
+      console.log(`UseEffect3 with interval number ${count} is running`);
+    }, 1000);
+
+    return () => {
+      console.log(
+        `UseEffect3 cleanup ran.\nsetInterval number ${count} is being cleared out`
+      );
+      clearInterval(myInterval);
+    };
+
+  return (
+    <div>
+      {console.log("rendered or re-rendered")}
+      <h1>Main Component</h1>
+      <button onClick={handleClick}>ToggleOne</button>
+      <button onClick={handleClick2}>ToggleTwo</button>
+      <button onClick={() => setCount(count + 1)}>Count</button>
+    </div>
+  );
+};
+
+export default Main;
